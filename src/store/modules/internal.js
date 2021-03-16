577974b06c11fe32597c9aa29b1ff9dc9cc4eaab
andefined <dimitris.papaevagelou@andefined.com>
2021-03-16 15:55:01 +0200

    check for localstorage

--- src/store/modules/internal.js
+++ src/store/modules/internal.js
@@ -28,17 +28,19 @@ export default {
 	actions: {
 		async getGeo ({ commit }) {
 			try {
-				if (storageSVC.get('geo') !== null) {
-					storageSVC.remove('geo');
-				}
-				if (storageSVC.get('geo-2.1.0') !== null) {
-					storageSVC.remove('geo-2.1.0');
-				}
-				if (storageSVC.get('geo-2.2.0') !== null) {
-					storageSVC.remove('geo-2.2.0');
+				if (typeof window.localStorage !== 'undefined') {
+					if (storageSVC.get('geo') !== null) {
+						storageSVC.remove('geo');
+					}
+					if (storageSVC.get('geo-2.1.0') !== null) {
+						storageSVC.remove('geo-2.1.0');
+					}
+					if (storageSVC.get('geo-2.2.0') !== null) {
+						storageSVC.remove('geo-2.2.0');
+					}
 				}
 				const res = await internalSVC.getGeo();
-				if (isChrome()) storageSVC.set('geo-' + process.env.VUE_APP_VERSION, res);
+				if (isChrome() && typeof window.localStorage !== 'undefined') storageSVC.set('geo-' + process.env.VUE_APP_VERSION, res);
 				commit('setGeo', res);
 				return res;
 			} catch (error) {
